7f2964d1a6ff398692f011c7c3525f9731aff88a
James Sherlock <15193942+Sherlouk@users.noreply.github.com>
2023-01-23 14:58:51 +0000

    slowly unwind

--- Sources/Connection/StompConnection.swift
+++ Sources/Connection/StompConnection.swift
@@ -31,8 +31,8 @@ internal final class StompConnection {
         
         print("starting")
         let bootstrap = ClientBootstrap(group: eventLoopGroup)
-//            .channelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
-           .channelOption(ChannelOptions.socketOption(.so_keepalive), value: 1)
+           .channelOption(ChannelOptions.socketOption(.so_reuseaddr), value: 1)
+            // .channelOption(ChannelOptions.socketOption(.so_keepalive), value: 1) // BREAKS BUILD
             // .channelOption(ChannelOptions.socketOption(.tcp_nodelay), value: 1) // BREAKS BUILD
             .channelOption(ChannelOptions.maxMessagesPerRead, value: 16)
             .channelOption(ChannelOptions.recvAllocator, value: AdaptiveRecvByteBufferAllocator())
